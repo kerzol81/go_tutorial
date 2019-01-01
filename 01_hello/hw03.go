733a5c25b2dc08a480ce9964c87f36b53758fef2
kerzol81 <kerenyi.nagy.zoltan@gmail.com>
2019-01-01 18:20:01 +0100

    printing an integer as char

---
+++ 01_hello/hw03.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var i int = 66
+
+func main(){
+	fmt.Println(i)
+	fmt.Printf("Type: %T\n", i)
+	fmt.Printf("Test: %c\n", i)  // 66 is the ascii representation of the letter B
+	}
