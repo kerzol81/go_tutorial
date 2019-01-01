9caef96ce730f9f8e4b11da8bb4d31bda67c4c28
kerzol81 <kerenyi.nagy.zoltan@gmail.com>
2019-01-01 18:50:43 +0100

    variable assignments

--- 02_vars/vars.go
+++ 02_vars/vars.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 	/* MAIN TYPES
+	
 	string
 	bool
 	int int8 int16 int32 int64
@@ -11,24 +12,27 @@ import "fmt"
 	rune - alias for int32
 	float32 float64
 	complex64 complex128
+	
 	*/
 	
 	// variables must be  used, unless it will not compile
 
 func main(){
-	var i int = 8
-	var s string = "aaa"
+	var i int = 10
+	var s string = "AAAAAAA"
 	//const
 	const isCool = true
 	
-	// assignment
-	name, email := "ZOL", "zol@gmail.com"
+	// assignment, go will know that these are strings
+	name, email := "zol", "zol@gmail.com"
 	
-	//shorthand only inside a function:
+	//shorthand; only inside a function:
 	n := "zol" 
 	
+	//just to make sure to I used all variables:
 	fmt.Println(i, s, isCool, n, name, email)
-	// get the type of
+	
+	// get the type of:
 	fmt.Printf("%T\n", s)
 	fmt.Printf("%T\n", i)
 	fmt.Printf("%T\n", isCool)
