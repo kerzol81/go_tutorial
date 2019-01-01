7987e2cec08c9f7077c3ef68bc11e78528818116
kerzol81 <kerenyi.nagy.zoltan@gmail.com>
2019-01-01 22:38:00 +0100

    if - else; if - else if - else skeleton

---
+++ 06_conditionals/if_else_if_else.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var fish string = "brass"
+	
+	if fish == "pike" {
+		fmt.Println("the fish is a pike")
+	} else if fish == "perch" {
+		fmt.Println("the fish is a perch")
+		} else {
+			fmt.Println("unknown fish")
+			} 	
+
+}
---
+++ 06_conditionals/if_else_syntax.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+	var a int = 10
+	var b int = 10
+	
+	
+	// if-else syntax:
+	
+	if a > b {								
+		fmt.Println("a is bigger than b")
+		
+	} else {								// esle must be placed this way
+			fmt.Println("tata")
+		}
+	
+	}
